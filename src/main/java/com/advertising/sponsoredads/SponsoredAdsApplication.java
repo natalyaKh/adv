a631c0cfd3d3f4f4346091a950b51b784c14a2a2
natalyaKh <khrebtiivski@gmail.com>
2021-06-17 15:41:52 +0300

    add H2 DB

---
+++ src/main/java/com/advertising/sponsoredads/SponsoredAdsApplication.java
@@ -0,0 +1,25 @@
+package com.advertising.sponsoredads;
+
+//import com.advertising.sponsoredads.model.Category;
+//import com.advertising.sponsoredads.model.Product;
+//import com.advertising.sponsoredads.repo.ProductRepository;
+//import org.apache.catalina.Store;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@SpringBootApplication
+public class SponsoredAdsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SponsoredAdsApplication.class, args);
+
+    }
+
+
+
+}
